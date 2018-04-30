10fa435fd1f7170af88b252fff6b1d022f63320a
QuirkySparkles <38667842+QuirkySparkles@users.noreply.github.com>
2018-04-30 22:15:54 +0300

    Add files via upload

---
+++ client/src/components/masterlist.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="list">
+        <div v-for="(master, index) in list" v-bind:key="master.id">
+            <p>{{master.name}} {{master.surname}} {{master.city}} Рейтинг: {{master.rating}}</p>
+            <input type="radio"
+                   v-bind:value="master.id"
+                   v-model="chosenmaster"
+                   v-on:click="chosenmaster=master.id, chooseidfn(chosenmaster)"
+                   v-bind:checked="master.id==chosenmaster">    
+            <label>Выбрать</label>
+        </div>
+    </div>
+</template>
+
+<script>    
+    export default {
+        name: "masterlist",
+        props: ['list', 'chooseidfn'],
+        data: function () {
+            return { chosenmaster: ''};
+        }
+    }
+</script>
