c1a49326fdf36bd66410a26bef4a4a5a73e3da0e
QuirkySparkles <38667842+QuirkySparkles@users.noreply.github.com>
2018-05-13 22:19:01 +0300

    Update index.js

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,11 +1,24 @@
-import App from '@/components/App';
+import Vue from 'vue';
+import Router from 'vue-router';
+import mainPage from '../components/mainpage.vue';
+import admin from '../components/admin.vue';
 
-var routes = [
-    {
-        path: '/',
-        name: 'Main',
-        component: App
-    }
-]
+Vue.use(Router)
 
-export default routes;
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'Main',
+            component: mainPage
+        },
+    
+        {
+            path: '/admin',
+            name: 'Admin',
+            component: admin
+        }
+    
+    ]
+})
