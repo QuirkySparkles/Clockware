310bbc2b1c4f8cc5edca8f3c1e94050357248a8f
QuirkySparkles <38667842+QuirkySparkles@users.noreply.github.com>
2018-05-13 22:22:34 +0300

    Update webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,18 +1,49 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
 
 module.exports = {
+    context: path.resolve(__dirname, './'),
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist',
+        publicPath: '/',
         filename: 'build.js'
+    },
+    devServer: {
+        contentBase: '/',
+        historyApiFallback: {
+            index: "/"
+        }
     },
     module: {
         rules: [
             {
                 test: /\.vue$/,
                 loader: 'vue-loader'
+            },
+            
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
             }
         ]
-    }
+    }, 
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': resolve('src')
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            file: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ]
 };
