5736fb9e971502ea21cb1784c59c2955526d4ea3
QuirkySparkles <38667842+QuirkySparkles@users.noreply.github.com>
2018-05-13 22:20:00 +0300

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,11 @@
-import Vue from 'vue'
-import App from './components/App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import routes from './routes';
+import vueRouter from 'vue-router';
 
 new Vue({
     el: '#app',
-    data: {
-        user: {
-            name: 'Tom',
-            email: ''
-        }
-    },
-//    router: router,
-    render: h => h(App)
+    router: routes,
+    template: '<App/>',
+    components: { App }
 })
