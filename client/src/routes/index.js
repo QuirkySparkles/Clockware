c7e1218c46862ce3b160aac9c1407f9ed5e40108
QuirkySparkles <38667842+QuirkySparkles@users.noreply.github.com>
2018-05-22 22:31:37 +0300

    Update index.js

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import mainPage from '../components/mainpage.vue';
 import admin from '../components/admin.vue';
+import masters from '../components/masters.vue';
+import clients from '../components/clients.vue';
+import cities from '../components/cities.vue';
+import reservations from '../components/reservations.vue';
 
 Vue.use(Router)
 
@@ -17,8 +21,28 @@ export default new Router({
         {
             path: '/admin',
             name: 'Admin',
-            component: admin
+            component: admin,
+            children: [
+                {
+                    path: 'masters',
+                    component: masters
+                },
+        
+                {
+                    path: 'clients',
+                    component: clients
+                },
+        
+                {
+                    path: 'cities',
+                    component: cities
+                },
+        
+                {
+                    path: 'reservations',
+                    component: reservations
+                }
+            ]
         }
-    
     ]
-})
+});
