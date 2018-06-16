7f11f39667f5eb16086b10c19a9e36a9a15a38c2
QuirkySparkles <38667842+QuirkySparkles@users.noreply.github.com>
2018-06-16 20:18:06 +0300

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,15 +1,7 @@
 import axios from 'axios';
 
-/*
-var connection = axios.create({
-        baseURL: 'http://localhost:8081'
-    });
-*/
-
 export default () => {
     return axios.create({
         baseURL: 'http://localhost:8081'
     })
 }
-
-//export default connection;
