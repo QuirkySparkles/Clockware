cdf9c7fb1d4ca2262ab526dfa7a5afc9ecadd952
QuirkySparkles <38667842+QuirkySparkles@users.noreply.github.com>
2018-06-16 20:15:37 +0300

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import routes from './routes';
 import vueRouter from 'vue-router';
+import style from '../style.css';
 
 new Vue({
     el: '#app',
